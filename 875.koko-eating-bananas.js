d537219a52fd77d994dcec95109b48955682b3c5
songyu <songyu@tencent.com>
2022-05-07 17:18:56 +0800

    feat: update

---
+++ 875.koko-eating-bananas.js
@@ -0,0 +1,40 @@
+/**
+ * @param {number[]} piles
+ * @param {number} h
+ * @return {number}
+ */
+var minEatingSpeed = function (piles, h) {
+    let xLeft = 1;
+    let xRight = Math.max(...piles);
+    // 尽可能小的速度来满足h小时
+
+    while (xLeft < xRight) {
+        const xMid = xLeft + Math.floor((xRight - xLeft) / 2);
+        const midValue = getTimeBySpeed(piles, xMid);
+
+        if (midValue === h) {
+            xRight = xMid - 1;
+        } else if (midValue < h) {
+            xRight = xMid - 1;
+        } else if (midValue > h) { // 不满足则要增大速度
+            xLeft = xMid + 1
+        }
+    }
+
+    if (getTimeBySpeed(piles, xLeft) <= h) {
+        return xLeft
+    }
+
+    return xLeft + 1;
+};
+
+function getTimeBySpeed(piles, speed) {
+    let hours = 0;
+    for (let i = 0; i < piles.length; i++) {
+        hours += Math.ceil(piles[i] / speed)
+    }
+    return hours;
+}
+
+// console.log(minEatingSpeed([3, 6, 7, 11], 8));
+console.log(minEatingSpeed([30, 11, 23, 4, 20], 6));
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,5 +1,1 @@
-5);
-solution.pickIndex(6);
-solution.pickIndex(7);
-solution.pickIndex(8);
-solution.pickIndex(
+xMid
